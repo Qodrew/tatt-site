71579ff295063e74be30653ce07a4981d139c6a9
Qodrew <120235371+Qodrew@users.noreply.github.com>
2023-04-24 09:15:52 +0300

    Create RevievLib.js

---
+++ RevievLib.js
@@ -0,0 +1,17 @@
+const revievTitle = [
+    "Best and most talented tattoo professional ever",
+    "5 stars for the sacred center tattoo",
+    "Will be the first place I visit again when back in Los Angeles",
+    "Big Rob is the best. I'm happy what I find him",
+    "They're really gifted tattooist",
+];
+const revievDescr = [
+    "I jist got 3/4 sleeve from Mat Hobs. He is a freehand Polynesian master, the best in the states. He and his family provide a wonderful tattoo experience.",
+    "I just had my first Japanice tattoo on my chest to bicep. Samanta uses free hand which is amazing. She's gifted tattoo artist. You will be more tahn satisfied with the outcome. I hightly recommended Samanta without a doubt!",
+    "Most of my tattoo's are from Rob. He's a great men and I love his work. I come with a crappy drawning as I can't draw very well and he turns it into art. What  I like is that I can say what I'm seeing in my head, my vision and he turns it into what i want.",
+    "I saw Big Rob this past weekend. He's the best. He just finished some great work on my arm. I plan on going back to have him clean some other work up. Go see him, you will be happy.",
+    "I was looking to have same cleanup work done to a band on my leg, but didn't want to just go to some shap that would imitate wthat they consider a Old school style. So i was incredibly happy to find an artist like Mat in the area. Prices were really reasonable and you'll be more than satisfied with the final results.",
+];
+const revievAuthor = ["Malcom Gladwell", "Charles F.Lummis", "M.F. Moonzajer", "T. Scott Mcleod", "Mandy Hale"];
+
+export {revievTitle, revievDescr, revievAuthor};
